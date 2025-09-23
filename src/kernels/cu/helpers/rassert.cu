99543b9821d81bbc1431bcb79708a3c25ce515c8
Nikolai Poliarnyi <polarnick239@gmail.com>
2025-09-24 00:23:09 +0300

    cuda: device-side rassert -> curassert

--- src/kernels/cu/helpers/rassert.cu
+++ src/kernels/cu/helpers/rassert.cu
@@ -3,12 +3,12 @@
 #include "../../defines.h"
 
 #if RASSERT_ENABLED
-    #define rassert(condition, error_code)					                    \
+    #define curassert(condition, error_code)					                    \
             do {									            \
                     if (!(condition)) {						                    \
                             printf("rassert code=%d line=%d\n", error_code % 1000000000, __LINE__); \
                     }								                    \
             } while (false)
 #else
-    #define rassert(condition, error_code) // do nothing
+    #define curassert(condition, error_code) // do nothing
 #endif
