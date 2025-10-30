55e8968cc6a634382ad85fa4560e3b4270fa692f
Nikolai Poliarnyi <polarnick239@gmail.com>
2025-10-30 23:35:03 +0300

    FastRandom: new random_sorted_unique_values(min, max, count)

--- libs/base/libbase/fast_random.h
+++ libs/base/libbase/fast_random.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <set>
 #include <limits>
+
 #include <libbase/point.h>
 
 // See https://stackoverflow.com/a/1640399
@@ -36,6 +38,42 @@ public:
 
     point3uc nextColor() {
         return point3uc(next(0, 255), next(0, 255), next(0, 255));
+    }
+
+    std::vector<unsigned int> random_sorted_unique_values(unsigned int min_value, unsigned int max_value, unsigned int count) {
+        std::vector<unsigned int> values;
+
+        rassert(min_value + count - 1 <= max_value, 4567891231234, min_value, max_value, count);
+        if (min_value + count - 1 == max_value) {
+            for (unsigned int i = 0; i < min_value; ++i) {
+                values.push_back(min_value + i);
+            }
+        } else {
+            std::set<unsigned int> used;
+            unsigned int max_attempts = 100;
+            for (unsigned int i = 0; i < count; ++i) {
+                unsigned int value = 5467435;
+                for (int attempt = 0; attempt < max_attempts; ++attempt) {
+                    value = next(min_value, max_value);
+                    if (used.count(value) == 0) {
+                        used.insert(value);
+                        break;
+                    } else {
+                        rassert(attempt + 1 < max_attempts, 43567843965123, min_value, max_value, count, used.size()); // too much failed attempts
+                    }
+                }
+                rassert(used.size() == i + 1, 435672384312, min_value, max_value, count, used.size());
+                values.push_back(value);
+            }
+            rassert(used.size() == count, 346345243, min_value, max_value, count, used.size());
+            values = {used.begin(), used.end()};
+            for (int i = 1; i < count; ++i) {
+                rassert(values[i - 1] < values[i], 4356234453242);
+            }
+        }
+
+        rassert(values.size() == count, 45332784931, values.size(), count);
+        return values;
     }
 
 private:
